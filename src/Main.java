f96a4363678c133cd06361253c540f5c226e26f5
puravdatta-sudo <purav@team-electro.com>
2020-06-09 11:25:57 -0500

    adding methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+
 public class Main {
-    public static void main (String[] args) {
+    public static void main (String[] args) { //Basic Initialization
+        //Lets make a basic Banking app through command line
+        account account = new account();
         
+      account.getCurMoney();
     }
+
 }
---
+++ src/account.java
@@ -0,0 +1,18 @@
+public class account {
+    public account() {
+
+    }
+
+    public double getCurMoney(){ //retrieve current money
+        double money = 0;
+
+        return money;
+    }
+    public void addMoney(double money){ //adding moeny to account
+
+    }
+    public void withdrawMoney(double money){ //taking out money from the account
+
+    }
+
+}
