8df8e0c8a00e3f9d73638eeff5a858c8c8b269f9
puravdatta-sudo <purav@team-electro.com>
2020-06-09 11:30:53 -0500

    adding array to store movement

--- src/account.java
+++ src/account.java
@@ -1,4 +1,8 @@
 public class account {
+    double currentMoney;
+    int arraySize = 3000;
+    double[] moneyMovement = new double[arraySize];
+    double startingMoney = 100.0;
     public account() {
 
     }
@@ -14,5 +18,7 @@ public class account {
     public void withdrawMoney(double money){ //taking out money from the account
 
     }
+    public void currentTrend(double money){ //give an average trend of the account
 
+    }
 }
