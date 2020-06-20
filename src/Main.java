b6d750faeec45332a1987c75780cb4ffa9d2510c
Purav Datta <puravdatta@gmail.com>
2020-06-20 10:09:39 -0500

    adding spacing

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,24 @@ public class Main {
 
       if (entry.equals("addMoney")) {
         System.out.println("How Much?");
+        String moneytoadd = scanner.next();
+
+        try{
+
             double numMoney = Double.parseDouble(moneytoadd);
+
+            if(numMoney>0){
                 account.addMoney(numMoney);
             }
+            else {
+                System.out.println("Number is Negative!");
+            }
+
+        }
+        catch (Exception e){
+            System.out.println("Please enter a numeric value");
+        }
+
         System.out.println("Current Money: " + account.getCurMoney()); // printing current balance
       }
 
